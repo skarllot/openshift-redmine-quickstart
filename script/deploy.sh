9879ca54c5204e062ae025d4edde2b71cfd937d7
Fabrício Godoy <skarllot@gmail.com>
2016-03-19 03:22:23 -0300

    Fixed deploying script

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,11 +3,13 @@
 REPO_NAME=openshift-redmine-quickstart
 REPO_BRANCH=master
 
+# Remove all cartridge data
+rm -rf ./* .openshift/
+
 # Download from quickstart repository
 wget https://github.com/skarllot/${REPO_NAME}/archive/${REPO_BRANCH}.tar.gz
 
 # Replace current data to download data
-rm -rf ./* .openshift/
 tar xzf ${REPO_BRANCH}.tar.gz
 rm -f ${REPO_BRANCH}.tar.gz
 mv ${REPO_NAME}-${REPO_BRANCH}/* ${REPO_NAME}-${REPO_BRANCH}/.* ./ &>/dev/null
